3a8df905243728afc1cfa7509d6abe604b0c342f
onismo <onismomudavanhu@gmail.com>
2023-02-24 07:45:15 +0200

    fixed out of memory issue

--- src/main/java/zw/org/mohcc/renamedb/RenameDbApplication.java
+++ src/main/java/zw/org/mohcc/renamedb/RenameDbApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -28,60 +30,51 @@ public class RenameDbApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
 
-//        Path dir  = Paths.get("/home/onismo/Desktop/data");
-        Path dir  = Paths.get("/home/administrator/backups/duplicate_db_handler/test_dir/input");
+        Path dir = Paths.get("/home/onismo/Desktop/data");
+//        Path dir  = Paths.get("/home/administrator/backups/duplicate_db_handler/test_dir/input");
 
-        Map<String, Integer> counters  = new HashMap<>();
-        int[] counter  =  {0};
+        Map<String, Integer> counters = new HashMap<>();
+        int[] counter = {0};
+        File folder = new File("/home/onismo/Desktop/data");
+        File[] listOfFiles = folder.listFiles();
+        String s = "/*!40000 ALTER TABLE `laboratory_request_order` DISABLE KEYS */;";
+        assert listOfFiles != null;
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
 
-        Files.list(dir).forEach(path -> {
-            if(path.toFile().isFile()){
-                String content;
-                try {
-                    content = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        // process the line.
+                        if (line.startsWith(s)) {
+                            String sqlLine = br.readLine();
+                            String[] tokens = sqlLine.split(",");
+                            if (tokens.length > 0) {
+                                String name = (tokens[5] + "_" + tokens[6]).replace("'", "").replace(" ", "_");
+                                counters.putIfAbsent(name, 0);
+                                counters.put(name, counters.get(name) + 1);
+                                name = name + "_processed_" + counters.get(name) + ".sql";
+                                Files.move(file.toPath(), file.toPath().resolveSibling("out/"+name));
+                                log.debug(name);
 
+                                counter[0]++;
 
+                            }
 
-                String s = "/*!40000 ALTER TABLE `laboratory_request_order` DISABLE KEYS */;\n";
-                int index  = content.lastIndexOf(s);
-                int nextIndex  = content.indexOf( "\n", index + s.length() +  1);
-
-
-                if(index > -1 && nextIndex > -1){
-                    String sql = content.substring(index + s.length() + 1, nextIndex);
-                    String[] tokens = sql.split(",");
-                    if(tokens.length > 0){
-                        String name  = (tokens[5]+ "_"+tokens[6]).replace("'", "").replace(" ", "_");
-                        counters.putIfAbsent(name, 0);
-                        counters.put(name, counters.get(name) + 1);
-
-                        name  = name + "_processed_"+ counters.get(name) + ".sql";
-
-                        log.debug(name);
-
-                        counter[0]++;
-
-
-                        try {
-//                            Files.write(new File("/home/onismo/Desktop/data/out/"+ name).toPath(), content.getBytes(StandardCharsets.UTF_8));
-                            Files.write(new File("/home/administrator/backups/duplicate_db_handler/test_dir/output/"+ name).toPath(), content.getBytes(StandardCharsets.UTF_8));
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
+                            break;
                         }
+
 
                     }
                 }
-            }else{
-                log.debug("Not a file");
+
+
+            } else {
+                log.debug("not file");
             }
-
-
-        });
+        }
 
         Set<String> keySet = counters.keySet();
-        log.debug("Processed " + counter[0] + " sql files. However we have "+ keySet.size() + (keySet.size()==1 ? " database": "databases"));
+        log.debug("Processed " + counter[0] + " sql files. However we have " + keySet.size() + (keySet.size() == 1 ? " database" : "databases"));
     }
 }
